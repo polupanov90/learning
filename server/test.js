0378a704a368d5ffe9ca783b642695f77506feb8
n.polupanov <n.polupanov@polymatica.com>
2025-02-06 12:16:15 +0300

    vps

--- server/test.js
+++ server/test.js
@@ -5,6 +5,10 @@ const cors = require('cors')
 const app = express();
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('hello!')
+})
+
 app.use(express.static("public")); // статические файлы будут в папке public
 app.use(express.json()); // подключаем автоматический парсинг json
 app.use(bodyParser.urlencoded({ extended: true }));
